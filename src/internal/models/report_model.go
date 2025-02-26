40f758de72a880d51f4ab1797a632d74625b2b7b
Patrick Selin <patrick.selin@gmail.com>
2025-02-26 14:36:31 +0200

    feat(report): seperate request and report model, delete mock

--- src/internal/models/report_model.go
+++ src/internal/models/report_model.go
@@ -4,19 +4,22 @@ import (
 	"time"
 )
 
-type ReportRequest struct {
-	OrderIDs     []string `json:"order_ids" validate:"required,dive,uuid"`
-	ReportType   string   `json:"report_type" validate:"required,oneof=csv pdf both"`
-	IncludeItems bool     `json:"include_items"`
-}
+type ReportStatus string
 
+const (
+	StatusPending   ReportStatus = "Pending"
+	StatusCompleted ReportStatus = "Completed"
+	StatusFailed    ReportStatus = "Failed"
+)
 
-type Report struct {
-	ReportID     string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"report_id"`
-	FileURL      string    `json:"file_url"`
-	Status       string    `json:"status"`
-	ReportType   string    `json:"report_type"`
-	IncludeItems bool      `json:"include_items"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+type ReportMetadata struct {
+	ReportID     string       `json:"report_id" dynamodbav:"ReportID"`
+	UserID       string       `json:"user_id" dynamodbav:"UserID"`
+	OrderIDs     []string     `json:"order_ids" dynamodbav:"OrderIDs"`
+	ReportType   string       `json:"report_type" dynamodbav:"ReportType"`
+	IncludeItems bool         `json:"include_items" dynamodbav:"IncludeItems"`
+	Status       ReportStatus `json:"status" dynamodbav:"Status"`
+	S3Key        string       `json:"s3_key" dynamodbav:"S3Key"`
+	CreatedAt    time.Time    `json:"created_at" dynamodbav:"CreatedAt"`
+	UpdatedAt    time.Time    `json:"updated_at" dynamodbav:"UpdatedAt"`
 }
