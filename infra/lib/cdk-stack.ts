8ce197b8af774687dd768adb5d174210a109ebbe
Patrick Selin <patrick.selin@gmail.com>
2025-03-04 08:43:07 +0200

    refactor(project): big refactor, change project structure

--- infra/lib/cdk-stack.ts
+++ infra/lib/cdk-stack.ts
@@ -1,17 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { S3Resources } from "./s3";
+import { DynamoDBResources } from "./dynamodb";
+import { ReportLambdas } from "./lambdas";
+import { ApiGateway } from "./api-gateway";
 
 export class ReportGeneratorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // DynamoDB Table
-    // S3 Bucket
-    // Lambda Function for Report Generation
-    // Lambda Function for Report Status
-    // Grant permissions
-    // API Gateway
+    const s3Resources = new S3Resources(this, "S3Resources");
+    const dynamoDBResources = new DynamoDBResources(this, "DynamoDBResources");
 
+    const reportLambdas = new ReportLambdas(this, "ReportLambdas", {
+      s3Resources,
+      dynamoDBResources,
+    });
 
+    new ApiGateway(this, "ApiGateway", {
+      reportLambdas,
+    });
   }
 }
--- lambda/report-generator/main.go
+++ lambda/report-generator/main.go
@@ -1,27 +1,11 @@
-// lambda/main.go
+// lambda/report-generator/main.go
 package main
 
 import (
-	"context"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/patrick-selin/crm-app-report-generator-service/internal/handlers"
 )
 
-type Request struct {
-	ReportID string `json:"report_id"`
-}
-
-type Response struct {
-	Message string `json:"message"`
-}
-
-func handler(ctx context.Context, req Request) (Response, error) {
-	message := handlers.GenerateReport(req.ReportID)
-	return Response{
-		Message: message,
-	}, nil
-}
-
 func main() {
-	lambda.Start(handler)
+	lambda.Start(lambdahandlers.HandleRequest)
 }
